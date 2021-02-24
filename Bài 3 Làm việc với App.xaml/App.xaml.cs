f59783534ff363a84b0c34b0e3b1fbcd5b70edf5
Tran Thanh Binh <tranthanhbinh.thcsthanhson@gmail.com>
2021-02-24 10:45:29 +0700

    Commit ngày 24/2/2021

---
+++ Bài 3 Làm việc với App.xaml/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Bài_3_Làm_việc_với_App.xaml
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_Startup(object sender, StartupEventArgs e) //Hàm cần tạo. Làm bất cú thứ gì trong đây
+        {
+            try
+            {
+                MessageBox.Show("Đây là một MessageBox");
+                MainWindow wnd = new MainWindow();
+                wnd.Title = "Something else";
+                wnd.Show();
+            }
+            finally
+            {
+                MessageBox.Show("Bạn đã đóng");
+            }
+           
+        }
+    }
+}

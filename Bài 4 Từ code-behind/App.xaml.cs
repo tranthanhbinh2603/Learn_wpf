454a07bed9c80189fa7a9f060058f49a4956cb27
Tran Thanh Binh <tranthanhbinh.thcsthanhson@gmail.com>
2021-03-09 21:04:16 +0700

    Commit ngày 9/3/2021

---
+++ Bài 4 Từ code-behind/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Bài_4_Từ_code_behind
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}

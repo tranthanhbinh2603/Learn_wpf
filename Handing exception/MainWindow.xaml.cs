cbe71b14fb0306d018879fc4a988d2d9e83c1e15
Tran Thanh Binh <tranthanhbinh.thcsthanhson@gmail.com>
2021-03-16 11:48:43 +0700

    Commit ngày 16/3/2021 lần 1

---
+++ Handing exception/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows;
+
+namespace WpfTutorialSamples.WPF_Application
+{
+    public partial class ExceptionHandlingSample : Window
+    {
+        public ExceptionHandlingSample()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string s = null;
+            s.Trim();
+        }
+    }
+}

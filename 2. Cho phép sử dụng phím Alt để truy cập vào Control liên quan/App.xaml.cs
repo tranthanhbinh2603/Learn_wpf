3a58743643ff19be6bc34a8c4801b3e26ef68b0d
Tran Thanh Binh <tranthanhbinh.thcsthanhson@gmail.com>
2021-03-23 11:55:38 +0700

    Commit ngày 23/3/2021 lần 1

---
+++ 2. Cho phép sử dụng phím Alt để truy cập vào Control liên quan/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace _2.Cho_phép_sử_dụng_phím_Alt_để_truy_cập_vào_Control_liên_quan
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ 6. Định dạng với code behind/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _6.Định_dạng_với_code_behind
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            TextBlock tb = new TextBlock();
+            tb.TextWrapping = TextWrapping.Wrap;
+            tb.Margin = new Thickness(10);
+            tb.Inlines.Add("An example on ");
+            tb.Inlines.Add(new Run("the TextBlock control ") { FontWeight = FontWeights.Bold }); //Kiểu chữ đậm
+            tb.Inlines.Add("using ");
+            tb.Inlines.Add(new Run("inline ") { FontStyle = FontStyles.Italic }); //Kiểu chữ nghiêng
+            tb.Inlines.Add(new Run("text formatting ") { Foreground = Brushes.Blue }); //Màu xanh
+            tb.Inlines.Add("from ");
+            tb.Inlines.Add(new Run("Code-Behind") { TextDecorations = TextDecorations.Underline }); //Kiểu chữ gạch chân
+            tb.Inlines.Add(".");
+            this.Content = tb;
+        }
+    }
+}

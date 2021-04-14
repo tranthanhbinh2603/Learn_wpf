bf2a4c5edfb525454bffca3881a9023f15acc053
Tran Thanh Binh <tranthanhbinh.thcsthanhson@gmail.com>
2021-04-14 13:06:05 +0700

    Commit ngày 14/4/2021 lần 2
    Thêm 3 bài sau vào phần Button:
    - Định dạng nội dung nâng cao
    - Có hình ảnh
    - Padding Button

---
+++ 3. Định dạng nội dung nâng cao/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace _3.Định_dạng_nội_dung_nâng_cao
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
